864440a5da19e751042dac0fc794f803ba076747
Hemanathan A S <111952621+HEMANATHAN2k01@users.noreply.github.com>
2024-01-02 12:05:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { UseEffect } from "./state/UseEffect";
+// import { Parent } from "./props/Parent";
+// import { UseState } from "./state/UseState";
+
+
+function App() {
+  return <div>
+    {/* <UseState /> */}
+    {/* <Parent /> */}
+    <UseEffect />
+  </div>;
+}
+
+export default App;
